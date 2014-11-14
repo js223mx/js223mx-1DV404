db43a817a5747b8f2ad2ddde9265082c85937f78
js223mx <js223mx@student.lnu.se>
2014-11-14 16:11:28 +0100

    started on 1c

---
+++ NastStrorsta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NastStrorsta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int value;           
+            //string input = Console.ReadLine();
+            //int.TryParse(input, out value);
+            //int number = 0;
+            //int maxNumber = 0;
+
+            string input;
+            Console.Write("Mata in 10 heltal:");
+            input = Console.ReadLine();
+
+            for (int i = 0; i < 10; i++) 
+            {
+                
+            }
+            
+        }
+
+    }
+}
