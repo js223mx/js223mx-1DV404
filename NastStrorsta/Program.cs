1f0b89ecdafcc308686ece36dc1b72ae74e92f27
js223mx <js223mx@student.lnu.se>
2014-11-17 16:40:00 +0100

    added some if statements

--- NastStrorsta/Program.cs
+++ NastStrorsta/Program.cs
@@ -15,14 +15,48 @@ namespace NastStrorsta
             //int.TryParse(input, out value);
             //int number = 0;
             //int maxNumber = 0;
-
-            string input;
-            Console.Write("Mata in 10 heltal:");
-            input = Console.ReadLine();
+            int inputValue = 0;
+            int maxValue = 0;
+            int secMaxValue = 0;
+            int tempvar = 0;
+ 
+            int input;
+           // Console.Write("Mata in 10 heltal:");
+            //int.TryParse(input, out inputValue);
 
             for (int i = 0; i < 10; i++) 
             {
-                
+                Console.Write("Mata in {0} heltal: ", i + 1);
+                //läser in numren från tangentbordet
+                input = int.Parse(Console.ReadLine());
+
+                if (i == 0) 
+                {
+                    maxValue = tempvar;
+                        //i++;
+                        //secMaxValue= maxValue;
+                        //maxValue =inputValue;
+                }
+                else if (i == 1) 
+                {
+                    if (maxValue < tempvar) 
+                    {
+
+                    }
+                    else 
+                    {
+                        secMaxValue = tempvar;
+                    }
+                    
+                  
+                }
+                else 
+                {
+                    if (tempvar > secMaxValue && tempvar < maxValue) 
+                    {
+                        secMaxValue = tempvar;
+                    }
+                }
             }
             
         }
