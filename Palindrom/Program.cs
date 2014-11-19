aa23a2ac4e6aaff14fed1552b4d027fd0aa33ad4
js223mx <js223mx@student.lnu.se>
2014-11-19 14:37:14 +0100

    finished palindrome

---
+++ Palindrom/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in en sträng: ");
+            string input = Console.ReadLine();
+            string myString = "";
+
+            for (int i = input.Length - 1; i >= 0; i--) // kollar input strängens längd och går tillbaka åt andra hållet
+            {
+                myString += input[i]; // kollar ifall inmatningen är ett palindrom
+            }
+            if (myString == input) 
+            {
+                Console.WriteLine("{0} är ett palindrom.", input);
+            }
+            else 
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fel! {0} är inte ett palindrom.", input);
+                Console.ResetColor();
+            }
+        }
+    }
+}
