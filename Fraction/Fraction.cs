8c9d68683052b2e27cfa0b912d62f149277ac88f
js223mx <js223mx@student.lnu.se>
2014-11-19 16:32:52 +0100

    strated on fraction

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Fraction
+    {
+        private int _numerator;
+        private int _denominator;
+        private int number = 0;
+
+        public int getNumerator()
+        {
+            return _numerator;
+        }
+        public int getDenomirator() 
+        {
+            return _denominator;
+            (if _denominator == 0)
+            {
+
+            }
+        }
+        public int add() 
+        {
+
+        }
+        public int multiply() 
+        {
+
+        }
+        public bool isNegative() 
+        {   //om täljaren / nämnaren är mindre än 0 så retunera sant
+            if (_numerator / _denominator < 0) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+
+        }
+        public int isEqualTo() 
+        {
+
+        }
+       
+    }
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en täljare: ");
+            Console.WriteLine("Mata in en nämnare: ");
+        }
+    }
+}
