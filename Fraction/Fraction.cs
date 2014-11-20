fbe864140f12ecc6e67ba4d60deea5ac1f806c1f
js223mx <js223mx@student.lnu.se>
2014-11-20 17:09:34 +0100

    almost done just need to fix my isEqualTo

--- Fraction/Fraction.cs
+++ Fraction/Fraction.cs
@@ -10,31 +10,55 @@ namespace Fraction
     {
         private int _numerator;
         private int _denominator;
-        private int number = 0;
+        //private int number = 0;
 
-        public int getNumerator()
+        public int getNumerator
         {
-            return _numerator;
-        }
-        public int getDenomirator() 
-        {
-            return _denominator;
-            (if _denominator == 0)
+            get{return _numerator; }
+            set 
             {
-
+                _numerator = value;
             }
-        }
-        public int add() 
-        {
 
         }
-        public int multiply() 
+        public int getDenomirator 
         {
+            get{return _denominator; }
+            set
+            {
+                if( value == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    throw new ArgumentException ("Du kan inte ange en nämnare med värdet 0.");
+                    
+                }
+                _denominator = value;
+            }
+            
+        }
+        public Fraction(int Numerator, int Denominator) 
+        {
+            getNumerator = Numerator;
+            getDenomirator = Denominator;
+        }
+        // kollar varje bråk efter lika nämnare och plussar två bråktal, fann lösningen på http://www.codeproject.com/Articles/11971/Fractions-in-C
+        public static Fraction operator + (Fraction a, Fraction b) 
+        {
+        int Nume = a.getNumerator  * b.getDenomirator +b.getNumerator *a.getDenomirator;
+        int Denum =  a.getDenomirator *  b.getDenomirator;
+        return new Fraction( Nume, Denum );
 
+        }
+        //multiplicerar täljarna, multiplicerar nämnarna, Fann lösningen på http://www.codeproject.com/Articles/11971/Fractions-in-C
+        public static Fraction operator * (Fraction a, Fraction b) 
+        {
+            int Nume = a.getNumerator * b.getDenomirator;
+            int Denum = a.getDenomirator * b.getNumerator;
+            return new Fraction(Nume, Denum);
         }
         public bool isNegative() 
         {   //om täljaren / nämnaren är mindre än 0 så retunera sant
-            if (_numerator / _denominator < 0) 
+            if (_numerator < 0 || _denominator < 0) 
             {
                 return true;
             }
@@ -44,10 +68,23 @@ namespace Fraction
             }
 
         }
-        public int isEqualTo() 
+        public bool isEqualTo( Fraction left, Fraction right)
         {
-
+            return (decimal)left.getNumerator * right.getDenomirator == (decimal)right.getNumerator * right.getDenomirator;
+            //if (left.getDenomirator == right.getDenomirator && left.getNumerator == right.getNumerator)
+            //if (this.getDenomirator == left.getDenomirator && this.getNumerator == left.getNumerator)
+            //{
+              //  return true;
+            //}
+            //else 
+            //{
+              //  return false;
+            //}
+        
         }
-       
+        public override string ToString()
+        {
+            return string.Format("Ditt bråktal är {0}/{1}.", getNumerator, getDenomirator);
+        }
     }
 }
--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -10,8 +10,53 @@ namespace Fraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mata in en täljare: ");
-            Console.WriteLine("Mata in en nämnare: ");
+
+
+            //Test 1 kollar om det går att skriva in ett bråktal
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("--------Bråk test 1--------\n");
+            Console.ResetColor();
+            Fraction a = new Fraction(7,2);
+            Console.WriteLine(a.ToString());
+
+            //Test 2 kollar om det går att addera 2 bråktal
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n--------Bråk test 2--------\n");
+            Console.ResetColor();
+            Fraction c = new Fraction(7, 3);
+            Fraction d = new Fraction(7, 5);
+            Fraction cd = c + d;
+            Console.WriteLine(cd.ToString());
+
+            //Test 3 kollar om det går att multiplicera 2 bråktal
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n--------Bråk test 3--------\n");
+            Console.ResetColor();
+            Fraction cdd = c * d;
+            Console.WriteLine(cdd.ToString());
+
+            //Test 4 kollar om det går att mata in negativa bråktal
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n--------Bråk test 4--------\n");
+            Console.ResetColor();
+            Fraction e = new Fraction(-7, -2);
+            Console.WriteLine(e.isNegative());
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n--------Bråk test 5--------\n");
+            Console.ResetColor();
+            Fraction f = new Fraction(1, 2);
+            Fraction g = new Fraction(2, 4);
+            Console.WriteLine(f.isEqualTo(g));
+
+            //Test 6 kollar om nämnaren är en nolla och kastar i sådana fall ett undantag
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n--------Bråk test 6--------\n");
+            Console.ResetColor();
+            Fraction s = new Fraction(7, 0);
+            Console.WriteLine(s.ToString());
+            Console.ReadKey();
+
         }
     }
 }
